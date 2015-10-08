3135d59271956a53ed0ba0bd8e36d051ccaebc95
markedwardmurray <markedwardmurray@gmail.com>
2015-10-08 15:43:42 -0400

    solution: privatize view properties

--- objc-BlackJackViews/Classes/Controllers/FISBlackjackViewController.m
+++ objc-BlackJackViews/Classes/Controllers/FISBlackjackViewController.m
@@ -4,6 +4,44 @@
 
 @interface FISBlackjackViewController ()
 
+@property (weak, nonatomic) IBOutlet UILabel *winner;
+
+
+@property (weak, nonatomic) IBOutlet UILabel *houseScore;
+
+@property (weak, nonatomic) IBOutlet UILabel *houseCard1;
+@property (weak, nonatomic) IBOutlet UILabel *houseCard2;
+@property (weak, nonatomic) IBOutlet UILabel *houseCard3;
+@property (weak, nonatomic) IBOutlet UILabel *houseCard4;
+@property (weak, nonatomic) IBOutlet UILabel *houseCard5;
+
+@property (weak, nonatomic) IBOutlet UILabel *houseStayed;
+@property (weak, nonatomic) IBOutlet UILabel *houseBusted;
+@property (weak, nonatomic) IBOutlet UILabel *houseBlackjack;
+
+@property (weak, nonatomic) IBOutlet UILabel *houseWins;
+@property (weak, nonatomic) IBOutlet UILabel *houseLosses;
+
+
+@property (weak, nonatomic) IBOutlet UILabel *playerScore;
+
+@property (weak, nonatomic) IBOutlet UILabel *playerCard1;
+@property (weak, nonatomic) IBOutlet UILabel *playerCard2;
+@property (weak, nonatomic) IBOutlet UILabel *playerCard3;
+@property (weak, nonatomic) IBOutlet UILabel *playerCard4;
+@property (weak, nonatomic) IBOutlet UILabel *playerCard5;
+
+@property (weak, nonatomic) IBOutlet UILabel *playerStayed;
+@property (weak, nonatomic) IBOutlet UILabel *playerBusted;
+@property (weak, nonatomic) IBOutlet UILabel *playerBlackjack;
+
+@property (weak, nonatomic) IBOutlet UILabel *playerWins;
+@property (weak, nonatomic) IBOutlet UILabel *playerLosses;
+
+@property (weak, nonatomic) IBOutlet UIButton *deal;
+@property (weak, nonatomic) IBOutlet UIButton *hit;
+@property (weak, nonatomic) IBOutlet UIButton *stay;
+
 @property (strong, nonatomic) NSArray *houseCardViews;
 @property (strong, nonatomic) NSArray *playerCardViews;
 
